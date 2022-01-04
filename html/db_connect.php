447270a977fe775c5c19fd8e40a41047a72de757
Bekirov Timur <bekirovtimur@gmail.com>
2022-01-05 01:54:20 +0300

    many new scripts

---
+++ html/db_connect.php
@@ -0,0 +1,4 @@
+<?php
+ $connect = mysqli_connect("mysql", "root", "root", "nhl"); 
+ $query = '';
+ ?>
--- html/mytests2.php
+++ html/mytests2.php
@@ -13,18 +13,25 @@ $season_data = json_decode($season_json, true);
 $dates = $season_data['dates'];
 // Extracting row by row
                foreach($dates as $dates_row) {
-                    
-                    $query .=  "INSERT INTO dates VALUES ('".$dates_row["date"]."', '".$dates_row["games"][0]["gamePk"]."','".$dates_row["games"][0]["gameDate"]."'); "; 
+                    $each_date = $dates_row['date'];
+                    $each_date_json = file_get_contents("https://statsapi.web.nhl.com/api/v1/schedule?date=$each_date");
+                    $each_date_data = json_decode($each_date_json, true); 
+                    $games = $each_date_data['dates'][0]['games']; 
+                    // Database query to insert data 
+                    // into database Make Multiple 
+                    // Insert Query 
+                    foreach($games as $games_row) {
+                    $query .=  "INSERT INTO dates VALUES ('".$dates_row["date"]."', '".$games_row["gamePk"]."','".$games_row["gameDate"]."'); "; 
    
                     $table_data .= '
                     <tr>
                         <td>'.$dates_row["date"].'</td>
-                        <td>'.$dates_row["games"][0]["gamePk"].'</td>
-                        <td>'.$dates_row["games"][0]["gameDate"].'</td>
+                        <td>'.$games_row["gamePk"].'</td>
+                        <td>'.$games_row["gameDate"].'</td>
                     </tr>
                     '; // Data for display on Web page
                 }
-               
+               }
     ///*
                 if(mysqli_multi_query($connect, $query)) {
                     echo '<h3>Inserted JSON Data</h3><br />';
